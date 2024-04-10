f2b5359c628403c4c589d202e0925f2c700cd5ac
RafaelMVilla <rafael.march.v@gmail.com>
2024-04-10 07:57:53 -0300

    Primeira versão

---
+++ Programa.java
@@ -0,0 +1,34 @@
+public class Programa {
+    
+    public static void main(String[] args) {
+        
+        // lista.Exercicio01.executar();
+        // lista.Exercicio02.executar();
+        // lista.Exercicio03.executar();
+        // lista.Exercicio04.executar();
+        // lista.Exercicio05.executar();
+        // lista.Exercicio06.executar();
+        // lista.Exercicio07.executar();
+        // lista.Exercicio08.executar();
+        // lista.Exercicio09.executar();
+        // lista.Exercicio10.executar();
+        // lista.Exercicio11.executar();
+        // lista.Exercicio12.executar();
+        // lista.Exercicio13.executar();
+        // lista.Exercicio14.executar();
+        // lista.Exercicio15.executar();
+        // lista.Exercicio16.executar();
+        // lista.Exercicio17.executar();
+        // lista.Exercicio18.executar();
+        // lista.Exercicio19.executar();
+        // lista.Exercicio20.executar();
+        // lista.Exercicio21.executar();
+        // lista.Exercicio22.executar();
+        // lista.Exercicio23.executar();
+        // lista.Exercicio24.executar();
+        // lista.Exercicio25.executar();
+        // lista.Exercicio26.executar();
+
+
+    }
+}
